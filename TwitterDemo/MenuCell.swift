546fbef58f1a0dff6eed542ce5f6d1e5cc02e6a3
nyerneni <naresh_yerneni@intuit.com>
2017-04-23 23:15:23 -0500

    first cut

---
+++ TwitterDemo/MenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuCell.swift
+//  TwitterDemo
+//
+//  Created by Yerneni, Naresh on 4/23/17.
+//  Copyright © 2017 Yerneni, Naresh. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
